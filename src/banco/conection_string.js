0b88108942dbe4436ac140e99d611c2548ab00f4
JoaoFel1pe <jfbs@cin.ufpe.br>
2023-08-12 19:45:45 -0300

    criando conexão do banco

---
+++ src/banco/conection_string.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv');  // Importando a biblioteca dotenv
+const path = require('path');
+dotenv.config({ path: path.resolve(__dirname, '..', '..', '.env') }); // Carregando as variáveis de ambiente 
+
+// Função para criar a string de conexão
+function createConnectionString(host, username, password, database, port) {
+    return `postgres://${username}:${password}@${host}:${port}/${database}`;
+}
+
+const conString = createConnectionString(process.env.PGHOST, process.env.PGUSER, process.env.PGPASSWORD, process.env.PGDATABASE, process.env.PGPORT); // Criando a string de conexão
+
+module.exports = {
+
+    conString: conString // Exportando a string de conexão
+
+};
--- src/banco/conexao.js
+++ src/banco/conexao.js
@@ -1,27 +1,65 @@
-const pg = require("pg");
+const { Client } = require('pg');
+const func_conString = require("./conection_string");
 
-const conString = "pg://ecycle:Gh5SYQO4F-iIWRxOa361Eg@e-cycle.cmarkbil1dof.us-east-2.rds.amazonaws.com:5432/bd_ecycle";
-const client = new pg.Client({
-    connectionString: conString,
-    ssl: { rejectUnauthorized: false } // Ignorar a validação do certificado
-});
+class Database {
+    constructor() {
+        this.conString = func_conString.stringDeConexao;
+        this.client = new Client({
+            connectionString: this.conString,
+            ssl: { rejectUnauthorized: false }
+        });
+    }
 
-client.connect(async function(err) {
-    if (err) {
-        console.error("Erro ao conectar ao banco de dados:", err);
-    } else {
-        console.log("Conexão bem-sucedida ao banco de dados.");
-
+    async conectar() {
         try {
-            const query = "SELECT * FROM teste;";
-            const result = await client.query(query);
-
-            console.log("Registros da tabela 'teste':");
-            console.log(result.rows);
+            await this.client.connect();
+            console.log("Conexão bem-sucedida ao banco de dados.");
+            return "Connected";
         } catch (error) {
-            console.error("Erro ao buscar registros da tabela:", error);
-        } finally {
-            client.end(); // Lembre-se de encerrar a conexão quando terminar.
+            console.error("Erro ao conectar ao banco de dados:", error);
         }
     }
-});
+
+    async consultar(query) {
+        try {
+            const result = await this.client.query(query);
+            console.log("Registros da tabela:");
+            console.log(result.rows);
+        } catch (error) {
+            console.error("Erro ao executar a consulta:", error);
+        }
+    }
+
+    fecharConexao() {
+        this.client.end()
+            .then(() => {
+                console.log("Conexão fechada.");
+            })
+            .catch(error => {
+                console.error("Erro ao fechar a conexão:", error);
+            });
+    }
+
+}
+
+module.exports = Database;
+
+/*  -- Mock
+
+async function main() {
+    const db = new Database();
+
+    try {
+        await db.conectar();
+        console.log(c);
+        const query = "SELECT * FROM teste;";
+        await db.consultar(query);
+
+    } catch (error) {
+        console.error("Erro durante a execução:", error);
+    } finally {
+        db.fecharConexao();
+    }
+}
+
+main();*/
