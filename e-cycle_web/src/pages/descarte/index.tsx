1878023d1e258c9c0d99ed5d60f9c977034c1b43
Marcos Antonio <marcosantonio-10@live.com>
2023-09-19 20:46:52 -0300

    descarte desktop

--- e-cycle_web/src/pages/descarte/index.tsx
+++ e-cycle_web/src/pages/descarte/index.tsx
@@ -13,19 +13,43 @@ import {
   UploadInput,
 } from "./styles";
 import { Layout } from "@/components/Layout";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const DescarteSchema = z.object({
+  endereco: z.string().nonempty("Digite um endereço"),
+});
+
+type DescarteInput = z.infer<typeof DescarteSchema>;
 
 export default function Descarte() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<DescarteInput>({
+    resolver: zodResolver(DescarteSchema),
+  });
+  const onSubmit: SubmitHandler<DescarteInput> = (data: DescarteInput) => {
+    console.log(data);
+    reset();
+  };
   return (
     <Layout>
-      <FormContainer>
+      <FormContainer onSubmit={handleSubmit(onSubmit)}>
         <StyledTitle>Onde há algum descarte?</StyledTitle>
         <InputContainer>
-          <StyledLabel>Nome do material</StyledLabel>
-          <BaseInput placeholder="Digite o nome" /* {...register("name")} */ />
-          {/*  {errors.name && <ErrorSpan>{errors.name.message}</ErrorSpan>} */}
+          <StyledLabel>Endereço</StyledLabel>
+          <BaseInput
+            placeholder="Digite o endereço"
+            {...register("endereco")}
+          />
+          {errors.endereco && <ErrorSpan>{errors.endereco.message}</ErrorSpan>}
         </InputContainer>
 
-        <SubmitInput /* type="submit" */ value="Enviar" />
+        <SubmitInput type="submit" value="Enviar" />
       </FormContainer>
     </Layout>
   );
